6525d774b60a980e18baa97594bca70f0887690d
Douglas <dglslvsbr@gmail.com>
2024-06-14 10:59:20 -0300

    improving the difficulty system

--- SpaceshipGame/Game/Controllers/ManageDifficulty.cs
+++ SpaceshipGame/Game/Controllers/ManageDifficulty.cs
@@ -9,6 +9,7 @@ namespace SpaceshipGame.Game.Controllers
         private readonly ManageAsteroids _manageAsteroids;
         private readonly Label _score;
         private int score;
+        private int Difficulty = 100;
 
         public ManageDifficulty(Spaceship spaceship, ManageShots manageShots, ManageAsteroids manageAsteroids, Label score)
         {
@@ -28,54 +29,13 @@ namespace SpaceshipGame.Game.Controllers
 
         private void UpdateDifficulty()
         {
-            if (score > 2000)
+            if (score > Difficulty)
             {
-                _spaceship.Speed = 5f;
-                _manageShots.Speed = 5f;
-                _manageAsteroids.Speed = 2.4f;
-                _manageAsteroids.Quantity = 17;
-            }
-            else if (score > 1000 && score < 2000)
-            {
-                _spaceship.Speed = 3.9f;
-                _manageShots.Speed = 3.9f;
-                _manageAsteroids.Speed = 2.2f;
-                _manageAsteroids.Quantity = 16;
-            }
-            else if (score > 500 && score < 600)
-            {
-                _spaceship.Speed = 3.7f;
-                _manageShots.Speed = 3.7f;
-                _manageAsteroids.Speed = 2f;
-                _manageAsteroids.Quantity = 15;
-            }
-            else if (score > 400 && score < 500)
-            {
-                _spaceship.Speed = 3.6f;
-                _manageShots.Speed = 3.6f;
-                _manageAsteroids.Speed = 1.8f;
-                _manageAsteroids.Quantity = 14;
-            }
-            else if (score > 300 && score < 400)
-            {
-                _spaceship.Speed = 3.4f;
-                _manageShots.Speed = 3.4f;
-                _manageAsteroids.Speed = 1.6f;
-                _manageAsteroids.Quantity = 13;
-            }
-            else if (score > 200 && score < 300)
-            {
-                _spaceship.Speed = 3.2f;
-                _manageShots.Speed = 3.2f;
-                _manageAsteroids.Speed = 1.4f;
-                _manageAsteroids.Quantity = 12;
-            }
-            else if (score > 100 && score < 200)
-            {
-                _spaceship.Speed = 3f;
-                _manageShots.Speed = 3f;
-                _manageAsteroids.Speed = 1.2f;
-                _manageAsteroids.Quantity = 11;
+                _spaceship.Speed += 0.1f;
+                _manageShots.Speed += 0.1f;
+                _manageAsteroids.Speed += 0.1f;
+                _manageAsteroids.Quantity += 1;
+                Difficulty += 100;
             }
         }
     }
